9d8f3e96be76c66b13ef606f014d7a96d29d227e
Nolan Phillips <ncphillips.19@gmail.com>
2019-02-10 17:15:13 -0400

    Actually render the RenderProps demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import { HocDemo } from "./components/hoc-demo/HocDemo";
+import { RenderPropsDemo } from "./components/render-props-demo/RenderPropsDemo";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HocDemo />
+        <RenderPropsDemo />
       </div>
     );
   }
--- src/components/render-props-demo/RenderPropsDemo.tsx
+++ src/components/render-props-demo/RenderPropsDemo.tsx
@@ -4,14 +4,14 @@ import { LoadingScreen } from "../LoadingScreen";
 import { ErrorScreen } from "../ErrorScreen";
 import { UserInfo } from "../UserInfo";
 
-export const Sidebar = () => {
+export const RenderPropsDemo = () => {
   let logout = () => (window.location.href = "/logout");
 
   return (
     <WithUser email="bob@example.com">
       {user => {
         if (user.loading) return <LoadingScreen />;
-        if (user.error) return <ErrorScreen />;
+        if (user.error || !user.data) return <ErrorScreen />;
         return <UserInfo user={user.data!} logout={logout} />;
       }}
     </WithUser>
